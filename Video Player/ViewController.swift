7287d9f2db72395093b6aeb8459e65e300392f48
Alex_Kogut13 <o.kogut@devlight.io>
2022-02-08 10:51:55 +0200

    Initial Commit

---
+++ Video Player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Video Player
+//
+//  Created by Alex Kogut on 08.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
