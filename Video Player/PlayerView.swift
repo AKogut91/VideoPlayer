d23c28a354f59e9a5027b66d8b02f74b8a464247
Alex_Kogut13 <o.kogut@devlight.io>
2022-02-08 21:23:07 +0200

    PlayerView works

---
+++ Video Player/PlayerView.swift
@@ -0,0 +1,196 @@
+//
+//  PlayerView.swift
+//  Video Player
+//
+//  Created by Alex Kogut on 08.02.2022.
+//
+
+import UIKit
+import AVFoundation
+
+enum PlayerViewButtonState {
+    case play
+    case pause
+}
+
+enum PlayerButtonType {
+    case backward
+    case forward
+}
+
+protocol PlayerViewDelegate: AnyObject {
+    func playPauseAction(state: PlayerViewButtonState)
+    func duration(seconds: Float)
+    func timeElapsed(seconds: Float, sliderValue: Float)
+    func sliderMaximum(value: Float)
+}
+
+final class PlayerView: UIView {
+    
+    var player: AVPlayer? {
+        get {
+            return playerLayer.player
+        }
+        set {
+            playerLayer.player = newValue
+        }
+    }
+    
+    var delegate: PlayerViewDelegate?
+    
+    private var playerLayer: AVPlayerLayer {
+        return layer as! AVPlayerLayer
+    }
+    
+    override class var layerClass: AnyClass {
+        return AVPlayerLayer.self
+    }
+    
+    private var state: PlayerViewButtonState = .pause {
+        didSet {
+            switch state {
+            case .play:
+                delegate?.playPauseAction(state: .play)
+            case .pause:
+                delegate?.playPauseAction(state: .pause)
+            }
+        }
+    }
+    
+    private var timeObserverToken: Any?
+    private var playerItemContext = 0
+    private var playerItem: AVPlayerItem?
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        // Only handle observations for the playerItemContext
+        guard context == &playerItemContext else {
+            super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+            return
+        }
+        
+        if keyPath == #keyPath(AVPlayerItem.status) {
+            let status: AVPlayerItem.Status
+            if let statusNumber = change?[.newKey] as? NSNumber {
+                status = AVPlayerItem.Status(rawValue: statusNumber.intValue)!
+            } else {
+                status = .unknown
+            }
+            // Switch over status value
+            switch status {
+            case .readyToPlay:
+                print(".readyToPlay")
+                player?.play()
+                state = .play
+                delegate?.sliderMaximum(value: Float(player?.currentItem?.duration.seconds ?? 0.0))
+                
+            case .failed:
+                print(".failed")
+            case .unknown:
+                print(".unknown")
+            @unknown default:
+                print("@unknown default")
+            }
+        }
+    }
+    
+    // MARK: - Private Method
+    private func setUpAsset(with url: URL, completion: ((_ asset: AVAsset) -> Void)?) {
+        let asset = AVAsset(url: url)
+        getDurationVideo(asset: asset)
+        asset.loadValuesAsynchronously(forKeys: ["playable"]) {
+            var error: NSError? = nil
+            let status = asset.statusOfValue(forKey: "playable", error: &error)
+            switch status {
+            case .loaded:
+                completion?(asset)
+            case .failed:
+                print(".failed")
+            case .cancelled:
+                print(".cancelled")
+            default:
+                print("default")
+            }
+        }
+    }
+    
+    private func setUpPlayerItem(with asset: AVAsset) {
+        playerItem = AVPlayerItem(asset: asset)
+        playerItem?.addObserver(self, forKeyPath: #keyPath(AVPlayerItem.status), options: [.old, .new], context: &playerItemContext)
+        
+        DispatchQueue.main.async { [weak self] in
+            self?.player = AVPlayer(playerItem: self?.playerItem!)
+            self?.setupTimeObserver()
+        }
+    }
+    
+    private func getDurationVideo(asset: AVAsset) {
+        let asset = asset.duration
+        let duration = CMTimeGetSeconds(asset)
+        delegate?.duration(seconds: Float(duration))
+    }
+    
+    /*
+     Create a periodic observer to update the movie player time slider
+     during playback.
+     */
+    private func setupTimeObserver() {
+        let interval = CMTime(value: 1, timescale: 2)
+        timeObserverToken = player?.addPeriodicTimeObserver(forInterval: interval, queue: .main) { [unowned self] time in
+            let timeElapsed = Float(time.seconds)
+            let currentTime = Float(CMTimeGetSeconds(player?.currentTime() ?? CMTime.init()))
+            delegate?.timeElapsed(seconds: timeElapsed, sliderValue: currentTime)
+        }
+    }
+    
+    // MARK: - Internal Methods
+    func play(with url: URL) {
+        setUpAsset(with: url) { [weak self] (asset: AVAsset) in
+            self?.setUpPlayerItem(with: asset)
+        }
+    }
+    
+    func playerAction() {
+        switch state {
+        case .play:
+            player?.pause()
+            state = .pause
+        case .pause:
+            player?.play()
+            state = .play
+        }
+    }
+    
+    func set(newTime: CMTime) {
+        player?.seek(to: newTime, toleranceBefore: .zero, toleranceAfter: .zero)
+    }
+    
+    func seekBackwardForward(type: PlayerButtonType) {
+        guard let currentPlayer  = player else {
+            return
+        }
+        
+        let playerCurrentTime = CMTimeGetSeconds(currentPlayer.currentTime())
+        var newTime: Double
+        
+        switch type {
+        case .backward:
+            newTime = playerCurrentTime - 5
+            
+            if newTime < 0 {
+                newTime = 0
+            }
+            
+        case .forward:
+            newTime = playerCurrentTime + 5
+        }
+        
+        let updatedTime: CMTime = CMTimeMake(value: Int64(newTime * 1000 as Float64), timescale: 1000)
+        player?.seek(to: updatedTime, toleranceBefore: CMTime.zero, toleranceAfter: CMTime.zero)
+    }
+    
+    deinit {
+        playerItem?.removeObserver(self, forKeyPath: #keyPath(AVPlayerItem.status))
+        print("deinit of PlayerView")
+    }
+    
+}
--- Video Player/ViewController.swift
+++ Video Player/ViewController.swift
@@ -6,14 +6,100 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet private weak var playerView: PlayerView!
+    @IBOutlet private weak var playPauseButton: UIButton!
+    @IBOutlet private weak var durationLabel: UILabel!
+    @IBOutlet private weak var startTimeLabel: UILabel!
+    @IBOutlet private weak var timeSlider: UISlider!
+    
+     // MARK: - Properties
+    private let timeRemainingFormatter: DateComponentsFormatter = {
+        let formatter = DateComponentsFormatter()
+        formatter.zeroFormattingBehavior = .pad
+        formatter.allowedUnits = [.minute, .second]
+        return formatter
+    }()
 
+    private let videoURL =  "http://www.exit109.com/~dnn/clips/RW20seconds_2.mp4" //"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+    
+    // MARK: - Life Cycler
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+        setPlayer()
+        playVideo()
     }
 
+    // MARK: - Private Method
+    private func setPlayer() {
+        playerView.backgroundColor = .clear
+        playerView.delegate = self
+    }
+    
+    // MARK: - Internal Methods
+    func setupUI() {
+        startTimeLabel.text = "00:00"
+        durationLabel.text = "00:00"
+        timeSlider.value = 0.0
+    }
+    
+    func playVideo() {
+        guard let url = URL(string: videoURL) else { return }
+        playerView.play(with: url)
+    }
+    
+    func createTimeString(time: Float) -> String {
+        let components = NSDateComponents()
+        components.second = Int(max(0.0, time))
+        return timeRemainingFormatter.string(from: components as DateComponents)!
+    }
+    
+    // MARK: - IBActions
+    @IBAction func playPauseAction(_ sender: UIButton) {
+        playerView.playerAction()
+    }
+    
+    @IBAction func timeSliderDidChange(_ sender: UISlider) {
+        let newTime = CMTime(seconds: Double(sender.value), preferredTimescale: 600)
+        playerView.set(newTime: newTime)
+    }
+    
+    @IBAction func seekForward(_ sender: Any) {
+        playerView.seekBackwardForward(type: .forward)
+    }
 
+    @IBAction func seekBackward(_ sender: Any) {
+        playerView.seekBackwardForward(type: .backward)
+    }
 }
 
+// MARK: - PlayerViewDelegate
+extension ViewController: PlayerViewDelegate {
+    
+    func sliderMaximum(value: Float) {
+        timeSlider.maximumValue = value
+    }
+    
+    func timeElapsed(seconds: Float, sliderValue: Float) {
+        timeSlider.value = sliderValue
+        startTimeLabel.text = createTimeString(time: seconds)
+    }
+    
+    func duration(seconds: Float) {
+        durationLabel.text = createTimeString(time: seconds)
+    }
+    
+    func playPauseAction(state: PlayerViewButtonState) {
+        switch state {
+        case .play:
+            playPauseButton.setImage(UIImage(systemName: "pause.fill"), for: .normal)
+        case .pause:
+            playPauseButton.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        }
+    }
+}
